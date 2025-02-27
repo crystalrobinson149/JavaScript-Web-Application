f30996d1cc2f76901ec44aba57fde28a8e1b0a95
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-02-26 22:51:51 -0500

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,7 +1,1 @@
-/**
- * Updates the page title based on the selected category
- * @param {string} category - Selected category
- */
-export function updatePageTitle(category) {
-    document.title = `Fashion Trends - ${category.charAt(0).toUpperCase() + category.slice(1)}`;
-}
+
